7562fc0e1da8fddae8dde2e201bab5b015c7f584
Bennett Smrdel <bennett.smrdel@heysparkbox.com>
2022-09-26 16:28:30 -0500

    feat: Added character functionality
    - Rouge inherits from character and can use the talk function.
    - Rouge takes 5 damage every 5th attack.
    - Will counter attack with 5 damage for every enemy miss.
    - Only deals 1 damage back to enemy once hit.
    - Can say name in all caps.
    - Can cast protection for 3 turns.
    - All attack edge cases have been met.
    
    FSA22V2-99

--- src/rogue/index.js
+++ src/rogue/index.js
@@ -1,10 +1,23 @@
 import Character from '../character';
 
-class Rogue {
+class Rogue extends Character {
+  #timesAttacked = 0;
+
   talk() {
+    return `hello. my name is ${this.name}.`.toLowerCase();
   }
 
   attack() {
+    this.#timesAttacked += 1;
+
+    // Only able to get hit every 5th attack
+    if (this.#timesAttacked % 5 === 0) {
+      this.health -= 5;
+      return 1;
+    }
+
+    // When enemy misses, counter with 5 damage
+    return 5;
   }
 }
 
--- src/wizard/index.js
+++ src/wizard/index.js
@@ -1,10 +1,30 @@
 import Character from '../character';
 
-class Wizard {
+class Wizard extends Character {
+  #timesAttacked = 0;
+  #protection = 3;
+
   talk() {
+    return `HELLO! MY NAME IS ${this.name.toUpperCase()}.`;
   }
 
   attack() {
+    this.#timesAttacked += 1;
+
+    if (this.#protection > 0) {
+      // When protection spell is active, don't take damage and deal double damage back.
+      this.#protection -= 1;
+      return 2;
+    }
+
+    // Take 10 damage in all other cases
+    if (this.#protection <= 0) this.health -= 10;
+
+    // Cast protection if this is the first attack
+    if (this.#timesAttacked === 1) {
+      this.#protection = 3;
+    }
+    return 1;
   }
 }
 
